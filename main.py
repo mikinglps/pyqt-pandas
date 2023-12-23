60d2b13e3922fc1b27ded1e3bbf9115745a6df2a
Michel Lopes <micheloblopes@gmail.com>
2023-12-22 21:24:12 -0300

    .

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ class ButtonHandler:
         self._button = QPushButton("Load File")
         self._layout.addWidget(self._button) 
         self._button.clicked.connect(lambda: self.on_clicked())
+        self._isActive = False;
+        self._table = QTableWidget()
 
     def on_clicked(self):
         filename = QFileDialog.getOpenFileName()
@@ -17,15 +19,21 @@ class ButtonHandler:
         self.show_table()
     
     def show_table(self):
-        table = QTableWidget()
+        if self._isActive is True:
+            self._layout.removeWidget(self._table)
+            self._table.deleteLater()
+            self._table = QTableWidget()
+            self._isActive = False
+        
         for i in range(len(self._df['#'].astype('int'))):
-            table.insertRow(i)
+            self._table.insertRow(i)
             for k in range(12):
-                table.setColumnCount(12)
-                table.setHorizontalHeaderLabels(self._df.columns)
-                table.setItem(i, k, QTableWidgetItem(self._df.iloc[[i],[k]].to_string(index=False, header=False)))
-        self._layout.addWidget(table)
-        table.show()
+                self._table.setColumnCount(12)
+                self._table.setHorizontalHeaderLabels(self._df.columns)
+                self._table.setItem(i, k, QTableWidgetItem(self._df.iloc[[i],[k]].to_string(index=False, header=False)))
+        self._layout.addWidget(self._table)
+        self._table.show()
+        self._isActive = True;
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
